04ddbd05c7e8ce72260512f3d390b24b7b62e395
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-09-15 20:32:09 +0530

    Update Q1.c

--- Day-1/Q1.c
+++ Day-1/Q1.c
@@ -1,1 +1,16 @@
+/*Write a program to input two numbers and display their sum.*/
+#include <stdio.h>
+int main()
+{
+    int a,b;//declaration
+     printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+    printf("enter the value a = "); 
+    scanf("%d",&a);
+    printf("enter the value b = "); 
+    scanf("%d",&b);
+    int sum=0;
+    sum = a + b;
+    printf("%d",sum);
+    return 0;
+}
 
