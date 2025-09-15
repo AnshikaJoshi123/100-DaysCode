1f634db3c92848b9b20501adb1d80b83c2ca75ef
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-09-15 21:04:15 +0530

    Update Q5.c

--- Day-3/Q5.c
+++ Day-3/Q5.c
@@ -1,1 +1,13 @@
-
+/*Write a program to convert temperature from Celsius to Fahrenheit.*/
+#include<stdio.h>
+int main()
+{
+	float celsius,fahrenheit;
+          printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+	printf("Enter the temperature in celsius");
+	scanf("%f",&celsius);
+	fahrenheit=(celsius*9/5)+32;
+	printf("The temperature in fahrenheit is=%f",fahrenheit);
+	
+	return 0;
+} 
