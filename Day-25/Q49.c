2487ac9eb813c8daddd82d18b6da05e22198a29b
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-10-28 14:47:30 +0530

    Create Q49.c

---
+++ Day-25/Q49.c
@@ -0,0 +1,27 @@
+/*Q49 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+🔄*/
+#include <stdio.h>
+int main() {
+    printf("Name - Anshika Joshi\n");
+    printf("Sap id - 590028351\n");
+    printf("Batch - B1\n");
+    printf("Course - Bsc-CS\n");
+    printf("---------------------------------------------------------------\n");
+
+    int i, j;
+
+    for (i = 5; i >= 1; i--) {
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
