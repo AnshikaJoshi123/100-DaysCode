dd33a428d15e5e8a878007d6dd14790297603254
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-10-28 14:40:44 +0530

    Create Q46.c

---
+++ Day-23/Q46.c
@@ -0,0 +1,29 @@
+/*Q46 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+#include <stdio.h>
+int main() {
+    printf("Name - Anshika Joshi\n");
+    printf("Sap id - 590028351\n");
+    printf("Batch - B1\n");
+    printf("Course - Bsc-CS\n");
+    printf("---------------------------------------------------------------\n");
+
+    int i, j;
+
+    // Outer loop for rows
+    for (i = 1; i <= 5; i++) {
+        // Inner loop for columns
+        for (j = 1; j <= 5; j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to next line after each row
+    }
+
+    return 0;
+}
